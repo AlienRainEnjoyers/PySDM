a92718bbcea388da0938a0bf8b9fa92f9e44ec74
lursz <lukoszr@gmail.com>
2025-05-25 00:07:18 +0200

    tests I guess

---
+++ examples/PySDM_examples/Alien_Rain/alien_rain.py
@@ -0,0 +1,19 @@
+def calculate_raindrop_parameters(planet):
+    """
+    Calculates initial raindrop size parameters.
+    Returns the minimum raindrop size, maximum stable raindrop size, and the size step.
+    # return r_small, r_max, dr
+    """
+    pass
+
+def compute_evaporation_and_min_radius(pl_initial, R_p, M_p, X_composition, r_small, r_max, dr):
+    """
+    Computes the fraction of mass evaporated and minimum raindrop size for various RH values.
+    """
+    pass
+
+def main():
+    """
+    Main function to run the raindrop size calculations.
+    """
+    pass
