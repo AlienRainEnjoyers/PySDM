46b17fda1f46e2735ac21471d1b525454667d4e3
lursz <lukoszr@gmail.com>
2025-06-07 16:16:12 +0200

    tests are now in tests

--- examples/PySDM_examples/Alien_Rain_2025/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-The Physics of Falling Raindrops in Diverse Planetary Atmospheres
-[Loftus, K., & Wordsworth, R. D. 2021 (Journal of Geophysical Research: Planets, 126)](https://doi.org/10.1029/2020JE006653)
-
-"""
