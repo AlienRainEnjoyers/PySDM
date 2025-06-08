db7ee1dffac09d916331b0ba447e4f9bfa85cfac
Hevagog <qkawiak@gmail.com>
2025-06-08 11:30:23 +0200

    fix ground_truth generation script

--- tests/examples_tests/Loftus_and_Wordsworth_2021/ground_truth/gen_figure.py
+++ tests/examples_tests/Loftus_and_Wordsworth_2021/ground_truth/gen_figure.py
@@ -4,14 +4,15 @@
 ################################################################
 import numpy as np
 import matplotlib.pyplot as plt
+import os 
 
 # load results
-dir = 'output/fig02/'
-RHs = np.load(dir+'RHs.npy')
-r0grid = np.load(dir+'r0grid.npy')
-RHgrid  = np.load(dir+'RHgrid.npy')
-m_frac_evap = np.load(dir+'m_frac_evap.npy')
-r_mins = np.load(dir+'r_mins.npy')
+dir = os.path.dirname(os.path.abspath(__file__))
+RHs = np.load(os.path.join(dir, 'RHs.npy'))
+r0grid = np.load(os.path.join(dir, 'r0grid.npy'))
+RHgrid = np.load(os.path.join(dir, 'RHgrid.npy'))
+m_frac_evap = np.load(os.path.join(dir, 'm_frac_evap.npy'))
+r_mins = np.load(os.path.join(dir, 'r_mins.npy'))
 
 i_RH75 = 29 # index for RH=0.75
 
@@ -47,7 +48,7 @@ cb.add_lines(c_10)
 axs[1,0].clabel(c_10, c_10.levels, fmt={0.1:'10% mass evaporated'},fontsize='smaller')
 axs[1,0].fill_betweenx(RHs,1e-2,(r_mins-1e-6)*1e3,edgecolor='k',facecolor='w',hatch='//')
 axs[1,0].annotate(text='TOTAL \\n EVAPORATION',xy=(0.04,0.55),c='k',backgroundcolor='w')
-axs[1,0].annotate(text=r'$r_\\mathrm{min}$',xy=(0.35,0.27),c='darkviolet',backgroundcolor='w',size=8)
+axs[1,0].annotate(text=r'$r_\mathrm{min}$',xy=(0.35,0.27),c='darkviolet',backgroundcolor='w',size=8)
 
 axs[0,0].scatter(r_mins[i_RH75]*1e3,0.99,color='darkviolet',zorder=10)
 axs[0,0].axvline(r_mins[i_RH75]*1e3,lw=0.5,c='darkviolet',ls='--')
@@ -55,7 +56,9 @@ axs[0,0].plot(r0grid[0,:]*1e3,m_frac_evap[i_RH75,:],lw=2.05,c='k')
 axs[0,0].axhline(1,c='w',lw=3)
 axs[0,0].plot([1e-2,r_mins[i_RH75]*1e3],[1,1],c='k',lw=2.05,ls='--')
 axs[0,0].annotate(text='surface RH = 0.75',xy=(0.8,0.85),c='plum',size=8)
-axs[0,0].annotate(text=r'$r_\\mathrm{min}$',xy=(0.13,0.05),c='darkviolet',size=8)
+axs[0,0].annotate(text=r'$r_\mathrm{min}$',xy=(0.13,0.05),c='darkviolet',size=8)
 
-plt.savefig('figs/fig02.pdf',transparent=True,bbox_inches='tight',pad_inches=0.5)
+figs_path = os.path.join(dir,'figs')
+os.mkdir(figs_path) if not os.path.exists(figs_path) else None
+plt.savefig(os.path.join(figs_path,'fig02.pdf'),transparent=True,bbox_inches='tight',pad_inches=0.5)
 plt.close()
