85cbb2c623fe4cca751a3dd1df325a615ea87b6d
Piotr Kubala <pkubala@student.agh.edu.pl>
2025-06-10 14:47:59 +0200

    removed files rogers and yau

--- PySDM/physics/drop_growth/__init__.py
+++ PySDM/physics/drop_growth/__init__.py
@@ -5,4 +5,3 @@ Formulation of the coupled heat-moisture diffusion problem
 from .fick import Fick
 from .howell_1949 import Howell1949
 from .mason_1971 import Mason1971
-from .rogers_and_yau_1996 import RogersAndYau1996
